0a9e8eb91e046205e6f02af3b0486d2124e1f824
mahaoxiang <wpimhx@gmail.com>
2021-11-29 17:04:59 -0500

    Add analysis function

--- gridworld.py
+++ gridworld.py
@@ -1,11 +1,13 @@
 import pickle
 
-import game
+#import game
 import logging
 import networkx as nx
 import numpy as np
 import os
-
+import util
+import time
+from guppy import hpy
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.WARNING)
@@ -65,6 +67,7 @@ class GridworldNx:
         self.rows = rows
         self.cols = cols
         self.conn = conn
+        self.gw_dim = (self.rows, self.cols)
         self.graph = nx.MultiDiGraph()
         self._construct_gridworld()
 
@@ -83,9 +86,9 @@ class GridworldNx:
         state_list = []
         for r in range(self.rows):
             for c in range(self.cols):
-                state_list.append((r, c))
-                st_id = r * self.cols + c
-                self.graph.add_node(st_id)
+                for p in range(2):
+                    st_id = util.cell2uid(r, c, p, self.gw_dim)
+                    self.graph.add_node(st_id)
         
         if self.conn == 4:
             # Represent (-1, 0), (1, 0), (0, -1), (0, 1)
@@ -95,17 +98,25 @@ class GridworldNx:
             # Represent (-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)
             self.act_index = [0, 1, 2, 3, 4, 5, 6, 7]
             self.act = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)]
-        
-        for st in state_list:
+
+        stlist = list(G.nodes)
+        for st in stlist:
+            r, c, p = util.uid2cell(st, self.gw_dim)
             for a_id in self.act_index:
-                next_st = tuple(np.array(st) + np.array(self.act[a_id]))
-                if next_st in state_list:
-                    st_id = st[0] * self.cols + st[1]
-                    next_st_id = next_st[0] * self.cols + next_st[1]
+                gw_st = (r, c)
+                next_st = tuple(np.array(gw_st) + np.array(self.act[a_id]))
+                if self.in_Gridworld(next_st):
+                    p_ = (p+1)%2
+                    next_st_id = util.cell2uid(next_st[0], next_st[1], p_)
                     self.graph.add_edge(st_id, next_st_id, action=a_id)
                     
         logger.info("Successfully Get NetworkX Graph")
-        
+
+    def in_Gridworld(self, gw_st):
+        if gw_st[0] <= 0 or gw_st[0] >=self.rows or gw_st[1] <= 0 or gw_st[1] >= self.cols:
+            return False
+        return True
+
     def preExist(self, st):
         pre = [pred for pred in self.graph.predecessors(st)]
         result = []
@@ -131,9 +142,19 @@ class GridworldNx:
             if actset == set(self.act_index):
                 result_set.add(pred)
         return result_set
+
+def Abalysis_nx(rows, cols):
+    start_time = time.time()
+    game_nx = GridworldNx(rows = i, cols = i)
+    nx_time = time.time()- start_time
+    print("--- %s seconds ---" % (nx_time))
+    h = hpy()
+    print(h.heap)
+
+    return game_nx
 if __name__ == '__main__':
 
     for i in range(2, 20):
-        game_bdd = Gridworld(rows=i, cols=i)
-        game_nx = GridworldNx(rows=i, cols=i)
-        print(f"gw size: {i, i}, BDD size: {game_bdd.size()}, NX size: {game_nx.size()}")
+#s        game_bdd = Gridworld(rows=i, cols=i)
+        game_nx = Analysis_nx(rows=i, cols=i)
+#        print(f"gw size: {i, i}, BDD size: {game_bdd.size()}, NX size: {game_nx.size()}")
